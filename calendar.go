700e580be758a0bf570ead2ac15d31c597140cb0
hsldymq <hsldymq@qq.com>
2020-02-29 01:51:29 +0800

    实现计算儒略日的算法

--- calendar.go
+++ calendar.go
@@ -2,6 +2,7 @@ package chinese_calendar
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -33,17 +34,51 @@ func init() {
 	sexagenaryDayBase = time.Date(1949, 10, 1, 0, 0, 0, 0, baseTimezone)
 }
 
-// Sexagenary 计算从甲子到指定跨度的干支纪元值
+// Sexagenary 根据给定的干支索引计算指定跨度的干支纪数
 // 用于计算干支纪时,日,月,年
+// span 指定跨越的数
+// startIdx 代表干支纪数序号, 0-59, 0代表甲子, 1代表乙丑, 以此类推
+//		不传默认为0
 // 例:
-//		当span == 2, 即甲子日往后推算2, 即"丙寅"
-//		当span == -3, 甲子日往前推算3, 即"辛酉"
+//		Sexagenary(2) 返回"丙寅", 即按照"甲子"往后移2: "甲子" -> "乙丑" -> "丙寅"
+//		Sexagenary(-3, 1) 返回"壬戌", 即按照"乙丑往前移3: "乙丑" -> "甲子" -> "癸亥" -> "壬戌"
 // 因为60干支周的计算实际上是模算数,所以参数中任何小于0或大于59的数都会换算成模60的数再计算干支纪日
-func Sexagenary(span int) string {
+func Sexagenary(span int, startIdx ...int) string {
+	sIdx := 0
+	if len(startIdx) > 0 {
+		sIdx = startIdx[0]
+	}
+
+	span += sIdx
 	if span < 0 {
 		span = span%60 + 60
 	}
 	span = span % 60
 
 	return fmt.Sprintf("%s%s", celestialStem[span%10], terrestrialBranch[span%12])
+}
+
+// julianDay 计算给定时间所对应的儒略日
+// 算法参考Jean Meeus的<<Astronomical Algorithms>>第二版中的公式
+// 只能用于1582年10月15日及以后的时间, 因为在这天之前需要用儒略历来计算儒略日, 而golang的时间计算方式始终按照格里历计算.
+func julianDay(t time.Time) float64 {
+	t.In(time.FixedZone("", 0))
+	year := float64(t.Year())
+	month := float64(t.Month())
+	secondsOfDay := float64(t.Hour()*3600) + float64(t.Minute()*60) + float64(t.Second())
+	if t.Month() < 3 {
+		year -= 1
+		month += 12
+	}
+	a := math.Floor(year / 100)
+	b := math.Floor(2 - a + math.Floor(a/4))
+	d := float64(t.Day()) + secondsOfDay/86400
+	//if year < 1000 {
+	//	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	//	b = 0
+	//}
+
+	return math.Floor(365.25*(year+4716)) +
+		math.Floor(30.6001*(month+1)) +
+		d + b - 1524.5
 }
--- calendar_test.go
+++ calendar_test.go
@@ -2,6 +2,7 @@ package chinese_calendar
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -19,10 +20,42 @@ func TestSexagenary(t *testing.T) {
 
 		for i := 0; i < 60; i++ {
 			require.Equal(t, sexagennary[i], Sexagenary(i))
+			require.Equal(t, sexagennary[i], Sexagenary(i-3, 3))
 			require.Equal(t, sexagennary[i], Sexagenary(i+60))
 			require.Equal(t, sexagennary[i], Sexagenary(i-60))
 			require.Equal(t, sexagennary[i], Sexagenary(i-120))
 		}
 	})
 
+}
+
+func TestJulianDayNumber(t *testing.T) {
+	t.Run("haha", func(t *testing.T) {
+		// 测试用例来自于<<Astronomical Algorithms>>第二版
+		cases := map[float64][6]int{
+			2451545.0:  {2000, 1, 1, 12, 0, 0},
+			2451179.5:  {1999, 1, 1, 0, 0, 0},
+			2446822.5:  {1987, 1, 27, 0, 0, 0},
+			2446966.0:  {1987, 6, 19, 12, 0, 0},
+			2447187.5:  {1988, 1, 27, 0, 0, 0},
+			2447332.0:  {1988, 6, 19, 12, 0, 0},
+			2436116.31: {1957, 10, 4, 19, 26, 24},
+			2415020.5:  {1900, 1, 1, 0, 0, 0},
+			2305447.5:  {1600, 1, 1, 0, 0, 0},
+			2305812.5:  {1600, 12, 31, 0, 0, 0},
+			//2026871.8:  {837, 4, 10, 7, 12, 0},
+			//1842713.0: {333, 1, 27, 12, 0, 0},
+			//1676496.5: {-123, 12, 31, 0, 0, 0},
+			//1676497.5: {-122, 1, 1, 0, 0, 0},
+			//1356001.0: {-1000, 7, 12, 12, 0, 0},
+			//1355866.5: {-1000, 2, 29, 0, 0, 0},
+			//1355671.4: {-1001, 8, 17, 21, 36, 0},
+			//0.0:       {-4712, 1, 1, 12, 0, 0},
+		}
+
+		for expect, date := range cases {
+			require.Equal(t, expect, julianDay(time.Date(date[0], time.Month(date[1]), date[2], date[3], date[4], date[5], 0, time.UTC)))
+		}
+
+	})
 }
