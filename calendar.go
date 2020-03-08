766f22aed139f57b50753fe036aea1482a1a9337
hsldymq <hsldymq@qq.com>
2020-03-08 20:17:24 +0800

    update timezone name

--- calendar.go
+++ calendar.go
@@ -13,5 +13,5 @@ var sexagenaryDayBase time.Time
 func init() {
 	// 农历计算时区恒为北京时间(东经120°标准时)
 	// 这里没有通过loadLocation载入Asia/Shanghai, 而是用FixedZone来固定这个8小时的时差(尽管当前这两者当前是等同的)
-	baseTimezone = time.FixedZone("Beijing", 8*60*60)
+	baseTimezone = time.FixedZone("UTF+8", 8*60*60)
 }
